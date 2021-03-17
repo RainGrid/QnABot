980d68344cc47aa54d8b63af8381f689a459f1dc
Ярослав <yavoloh@mail.ru>
2021-03-17 15:03:24 +0500

    export to csv

--- src/scenes/questionnares/menus/qStats.ts
+++ src/scenes/questionnares/menus/qStats.ts
@@ -1,8 +1,14 @@
 import dayjs = require('dayjs');
 import { createBackMainMenuButtons, MenuTemplate } from 'telegraf-inline-menu';
-import { QuestionnareAttemptModel } from '../../../models';
+import {
+  Answer,
+  QuestionModel,
+  QuestionnareAttempt,
+  QuestionnareAttemptModel,
+} from '../../../models';
 import { TelegrafContext } from '../../../types';
 import { getQuestionnare } from './helpers';
+import fs = require('fs');
 
 const periods = ['day', 'week', 'month', 'year', 'alltime'];
 
@@ -51,6 +57,52 @@ qStatsMenu.choose('stP', periods, {
   },
   columns: 2,
   buttonText: (ctx, key) => ctx.i18n.t(key),
+});
+
+qStatsMenu.interact((ctx) => ctx.i18n.t('export'), 'qExport', {
+  joinLastRow: true,
+  do: async (ctx, path) => {
+    const q = await getQuestionnare(ctx);
+    if (q) {
+      const answers = await QuestionnareAttemptModel.aggregate<
+        QuestionnareAttempt & { answers: Answer[] }
+      >([
+        {
+          $match: { questionnare: q._id, isFinished: true },
+        },
+        {
+          $lookup: {
+            from: 'answers',
+            localField: '_id',
+            foreignField: 'attempt',
+            as: 'answers',
+          },
+        },
+      ]);
+      const questions = await QuestionModel.find({ questionnare: q }).sort({
+        sortOrder: 1,
+      });
+      const header =
+        [ctx.i18n.t('timestamp')]
+          .concat(questions.map((qu) => qu.name))
+          .join(';') + '\n';
+      const body = answers
+        .map((at) =>
+          [dayjs(at.updatedAt).format('DD.MM.YYYY HH:mm:ss')]
+            .concat(at.answers.map((an) => an.answer))
+            .join(';'),
+        )
+        .join('\n');
+      const response = header + body;
+      const filename = `./export_${q.name}_${dayjs().format(
+        'DD.MM.YYYY_HH_mm_ss',
+      )}.csv`;
+      fs.writeFileSync(filename, '\ufeff' + response, 'utf8');
+      await ctx.replyWithDocument({ source: filename });
+      fs.unlinkSync(filename);
+    }
+    return false;
+  },
 });
 
 qStatsMenu.manualRow(
